e6b57b6d628eaf13f354d51662e8e81ffe599a24
Matt Miller <matt.miller@dsu.edu>
2015-01-14 14:10:24 -0600

    Adding Comments to Looper

--- Looper.c
+++ Looper.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-int printer(int size)
+int printer(int size) // n^2
 {
-  	int x = 10;
-	for(int i = 0; i < size; i++)
+	//size = n
+  	int x = 10;// 1
+	// 1 (i = 0)
+	// 1 + 2n + 
+	for(int i = 0; i < size; i++) // 1/2 n(n+1) = n^2/2 + n/2
+		// (3 + 5 * (n + n-1 + n-2...) ) * n = 3n + 5*n*(n-1 + n-2 +...)
+		// = 3n + 5* 1/2 n(n+1)
+		// = 3n + 5/2 n^2 + 5/2n
 	{
-		for(int j = i; j < size; j++)
+		// 2
+		// 1 (j = i) 
+		for(int j = i; j < size; j++) //100 + 99 +98 +  
 		{
-			if(x + j > 10000)
+			// 2 (test & inc)
+			// if = 2
+			if(x + j > 10000)//1
 			{
-				x = 0;
+				x = 0; //1
 			}
-			x += j;
-		}
+			x += j;//1
+			// sum = 5
+		}// 5 * n + 5 * n-1 + 5 * n-2 == (3 + 5 * (n + n-1 + n-2...) ) * n
 	}
-	return x;
+	return x;//1
+	
+	
+	//	= 3n + 5/2 n^2 + 5/2n + 3
 }
 
 
